3fc05e356c4e842aa695a73cd5815c92dba610b2
Irene Nguyen <irene.m.nguyen@gmail.com>
2014-02-13 20:22:29 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Memochi::Application.config.secret_token = '5288b9337fd41c60ff96ea01a0cef0dbed991330c5b4ad73fca59d7145cd743d072e8f1aad72e3ad362ebc41b2dd3ebd7dff1683621554ed8eedd663efe12792'
