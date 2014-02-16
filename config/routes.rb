eb1c31f3e14501c81f91bdffcf5eb946f20c8020
Irene Nguyen <irene.m.nguyen@gmail.com>
2014-02-15 23:41:10 -0800

    changing selections to nav drop down

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Memochi::Application.routes.draw do
   root :to => "StaticPages#home"
+  
   resources :users
   resources :memos, :except => [:edit, :update]
   resource :session, :only => [:new, :create, :destroy]
